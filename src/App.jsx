61c09437ba4a6579218680e86aa4f5f275151f96
Arun Kumar M <arun2451.a2@gmail.com>
2024-03-20 09:22:04 +0530

    Added Bootstrap and AOS. Enabled ESLint.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-function App() {
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
+function App() {
+  useEffect(function () {
+    AOS.init();
+  }, []);
   return (
-    <h3>
-      Cook book using React Context API
-    </h3>
-  )
+    <>
+      <div>
+        <h3 className="text-center">Cook book using React Context API</h3>
+      </div>
+      <button className="btn btn-primary" data-aos="fade-in">
+        Animate
+      </button>
+    </>
+  );
 }
 
-export default App
+export default App;
